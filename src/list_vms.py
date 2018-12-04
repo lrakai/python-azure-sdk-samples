e60bd7dbb4969064335f6df8219d0799ece20345
Logan Rakai <lmrakai@gmail.com>
2018-12-03 22:52:42 -0700

    Sample code to list resources, vms

--- src/list_vms.py
+++ src/list_vms.py
@@ -1,10 +1,12 @@
 import os
+import json
 
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 
-LOCATION = 'southcentralus'
+RESOURCE_GROUP = 'cal-473'
+
 
 def get_credentials():
     subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
@@ -15,12 +17,24 @@ def get_credentials():
     )
     return credentials, subscription_id
 
+
 def list_vms():
     credentials, subscription_id = get_credentials()
-    resource_client = ResourceManagementClient(credentials, subscription_id)
-    compute_client = ComputeManagementClient(credentials, subscription_id)
 
-    result_list = resource_client.resource_groups.list()
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    # result_list = resource_client.resource_groups.list()
+    # result_list = list(result_list)
+    # print(json.dumps(result_list))
+
+    for item in resource_client.resources.list_by_resource_group(RESOURCE_GROUP):
+        print(item.name)
+
+    compute_client = ComputeManagementClient(credentials, subscription_id)
+    for vm in compute_client.virtual_machines.list(RESOURCE_GROUP):
+        print(vm.name)
+
+    return any(compute_client.virtual_machines.list(RESOURCE_GROUP))
 
 if __name__ == "__main__":
-    list_vms()
+    has_vms = list_vms()
+    print(has_vms)
