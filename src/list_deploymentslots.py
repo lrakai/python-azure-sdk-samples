ff8a66aa5a25b5cc08dad9e07bbb0113374865af
Logan Rakai <lmrakai@gmail.com>
2018-12-06 13:52:28 -0700

    List web app deployment slots

---
+++ src/list_deploymentslots.py
@@ -0,0 +1,22 @@
+import json
+
+from azure.mgmt.web import WebSiteManagementClient
+
+from config import CONFIG
+from credentials import credential_helper
+
+def list_webapps():
+    ''' List Azure App Service Plans '''
+    credentials, subscription_id = credential_helper.get_credentials()
+    resource_group = CONFIG['resource_group']
+
+    web_client = WebSiteManagementClient(credentials, subscription_id)
+    for webapp in web_client.web_apps.list_by_resource_group(resource_group):
+        if any(web_client.web_apps.list_slots(resource_group, webapp.name)):
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    has_webapps = list_webapps()
+    print(has_webapps)
