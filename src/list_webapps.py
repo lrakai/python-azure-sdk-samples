143e5b5255b2bbd4368f250cf8b29d91e76f5e16
Logan Rakai <lmrakai@gmail.com>
2018-12-06 13:29:53 -0700

    Proper web app listing logic

--- src/list_webapps.py
+++ src/list_webapps.py
@@ -5,18 +5,18 @@ from azure.mgmt.web import WebSiteManagementClient
 from config import CONFIG
 from credentials import credential_helper
 
-def list_asps():
+def list_webapps():
     ''' List Azure App Service Plans '''
     credentials, subscription_id = credential_helper.get_credentials()
     resource_group = CONFIG['resource_group']
 
     web_client = WebSiteManagementClient(credentials, subscription_id)
-    for asp in web_client.app_service_plans.list_by_resource_group(resource_group):
-        print(asp.name)
+    for webapp in web_client.web_apps.list_by_resource_group(resource_group):
+        print(webapp.name)
 
-    return any(web_client.app_service_plans.list_by_resource_group(resource_group))
+    return any(web_client.web_apps.list_by_resource_group(resource_group))
 
 
 if __name__ == "__main__":
-    has_asps = list_asps()
-    print(has_asps)
+    has_webapps = list_webapps()
+    print(has_webapps)
