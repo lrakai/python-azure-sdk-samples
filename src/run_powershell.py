9a9001a63482c26020ecac23cca49f2ac12a95e6
Logan Rakai <lmrakai@gmail.com>
2019-10-15 19:54:09 -0600

    Add powershell script sample

---
+++ src/run_powershell.py
@@ -0,0 +1,45 @@
+import json
+
+import re
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.compute import ComputeManagementClient
+from msrestazure.azure_exceptions import CloudError
+
+from config import CONFIG
+from credentials import credential_helper
+
+
+def run_powershell():
+    credentials, subscription_id = get_credentials(event)
+    resource_group = event['environment_params']['resource_group']
+
+    compute_client = ComputeManagementClient(credentials, subscription_id)
+
+    machines = [machine for machine in compute_client.virtual_machines.list(resource_group)]
+    if not any(machines):
+        return False
+    vm_name = machines[0].name
+
+    parameters = {
+        'command_id': 'RunPowerShellScript',
+        'script': [
+          'sqlcmd -S ca-lab-vm -E -Q "use calabs;select TOP 1 * from order_summaries" | SELECT -Last 1'
+        ]
+    }
+
+    try:
+        poller = compute_client.virtual_machines.run_command(resource_group, vm_name, parameters)
+        result = poller.result()
+    except CloudError as exception:
+        if 'Run command extension execution is in progress. Please wait for completion before invoking a run command.' in exception.message:
+            return False
+        print(exception)
+        raise exception
+
+    match = re.search("^\((\d+) rows affected\)$", result.value[0].message)
+    return match is not None and int(match.group(1)) > 0
+
+
+if __name__ == "__main__":
+    success = run_powershell()
+    print(success)
