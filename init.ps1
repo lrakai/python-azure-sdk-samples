fd66621d1c997a9c644b60584a64a1265f51fa4a
lrakai <lmrakai@gmail.com>
2020-05-15 21:08:36 -0600

    Use production requirements.txt

--- init.ps1
+++ init.ps1
@@ -1,4 +1,22 @@
+$BITBUCKET_USER="lrakai"
+$VCF_BITBUCKET_CLONE_URL="https://$BITBUCKET_USER@bitbucket.org/cloudacademy/labs-vcf-boilerplates.git"
+$VCF_BITBUCKET_PATH=".."
+$VCF_BITBUCKET_DIR="$VCF_BITBUCKET_PATH/labs-vcf-boilerplates"
+$CLOUD="azure" # valid values=aws|azure|gcp
+$REQUIREMENTS="$VCF_BITBUCKET_DIR/python3.6/$CLOUD.requirements.txt"
+
+If (!(test-path $CF_BITBUCKET_DIR)) {
+    git config credential.helper store # store bitbucket credential on disk
+    git clone $VCF_BITBUCKET_CLONE_URL $VCF_BITBUCKET_DIR
+} else {
+    git -C $VCF_BITBUCKET_DIR pull origin master
+}
+
+cp $REQUIREMENTS requirements.txt
+
+rm -rf venv # clean start
+
 virtualenv venv
 .\venv\Scripts\activate
-pip install Azure azure-storage pylint autopep8
-. .\secrets.ps1
+pip install pylint autopep8 # dev dependencies
+pip install -r requirements.txt # prod dependencies
